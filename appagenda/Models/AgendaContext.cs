e8330d24f935d6a018f866fccbf14dfd7a724e84
carlos <csanchezpc@gmail.com>
2020-01-06 00:10:22 -0500

    cambios

--- appagenda/Models/AgendaContext.cs
+++ appagenda/Models/AgendaContext.cs
@@ -8,11 +8,10 @@ namespace appagenda.Models
 {
     public class AgendaContext:DbContext
     {
-        public AgendaContext() : base("cnmyconexion")
-        {
-            
-        }
+        public AgendaContext() : base("cnmyconexion"){}
         public DbSet<Contacto> Contactos { get; set; }
         public DbSet<Telefono> Telefonos { get; set; }
+
+        /*Fuente tutorial canal That C# guy:  https://www.youtube.com/watch?v=cIZB1wNMPlY*/
     }
 }
